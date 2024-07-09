44f3a916277b5073936c71b7e2e0ff7f03357375
ritik457 <ritikbansal457@gmail.com>
2024-07-09 19:34:11 +0530

    adding

---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './Sidebar.css'
+
+import profile from '../images/profile.png'
+import 
+{BsCart3, BsGrid1X2Fill, BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, 
+  BsListCheck, BsMenuButtonWideFill, BsFillGearFill}
+ from 'react-icons/bs'
+
+function Sidebar({openSidebarToggle, OpenSidebar}) {
+  return (
+    
+    <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive": ""}>
+        
+        <div className='sidebar-title'>
+          
+            <div className='sidebar-brand w-100'>
+                <BsPeopleFill  className='icon_header'/> Hello Admin
+            </div>
+           
+            <span className='icon close_icon' onClick={OpenSidebar}>X</span>
+        </div>
+        <img src={profile} alt=""/>
+        <ul className='sidebar-list'>
+            <li className='sidebar-list-item'>
+                <a href="#">
+                    <BsGrid1X2Fill className='icon'/> Dashboard
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsMenuButtonWideFill className='icon'/> Request
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsListCheck className='icon'/> Response
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsPeopleFill className='icon'/> Vendor
+                </a>
+            </li>
+            
+        </ul>
+    </aside>
+  );
+}
+
+export default Sidebar;
