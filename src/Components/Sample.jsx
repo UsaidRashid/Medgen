bb8d7c6de60a1f21b01fb6bc8ee10c7ab2c5b5e5
UsaidRashid <rashidusaid2@gmail.com>
2024-07-09 14:19:20 +0530

    Adding sample

--- src/Components/Sample.jsx
+++ src/Components/Sample.jsx
@@ -3,7 +3,15 @@ import React from "react";
 export default function Sample(){
     return(
         <div>
-            <h1>Sample here</h1>
+            <div class="card" style={{width:'18rem'}}>
+  <img src="..." class="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+            <h1 className="">Sample here</h1>
         </div>
     );
 }
