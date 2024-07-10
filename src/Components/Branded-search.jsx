9f046c22dd46e9de60e051bb4940efdb6c3f5d6d
Saksham Agarwal <sakshamagarwal0507@gmail.com>
2024-07-10 12:53:16 +0530

    Updated Search Pages

--- src/Components/Branded-search.jsx
+++ src/Components/Branded-search.jsx
@@ -1,31 +1,19 @@
 import React from 'react';
 import './Branded-search.css';
-import image from './Bg.png';
 
-const Branded = () => {
-    const style = {
-        backgroundImage: `url(${image})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        height: '100vh',
-        width: '100vw',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        opacity: '0.7',
-        
-    };
+export default function Branded(){
+
     return (
-        <>
-            <div className="search-bar-container" style={style}>
-                <input type="text" placeholder="Search Branded Medicine Here..." className="search-input" />
-                <button className="search-button">
-                    <b>Search</b>
-                    <i className="fa fa-search" />
-                </button>
+        <div>
+            <div className='image'>
+                <div className="search-bar-container">
+                    <input type="text" placeholder="Search Branded Medicine Here..." className="search-input" />
+                    <button className="search-button">
+                        <b>Search</b>
+                        <i className="fa fa-search" />
+                    </button>
+                </div>
             </div>
-        </>
+        </div>
     );
 };
-
-export default Branded;
--- src/Components/Generic-search.jsx
+++ src/Components/Generic-search.jsx
@@ -1,30 +1,19 @@
 import React from 'react';
 import './Generic-search.css';
-import image from './Bg.png';
 
-const Generic = () => {
-    const style = {
-        backgroundImage: `url(${image})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        height: '100vh',
-        width: '100vw',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        opacity: '0.7',
-    };
+export default function Generic() {
+
     return (
-        <>
-            <div className="search-bar-container-dark" style={style}>
-                <input type="text" placeholder="Search Generic Medicine Here..." className="search-input" />
-                <button className="search-button">
-                    <b>Search</b>
-                    <i className="fa fa-search" />
-                </button>
+        <div>
+            <div className='image'>
+                <div className="search-bar-container">
+                    <input type="text" placeholder="Search Generic Medicine Here..." className="search-input" />
+                    <button className="search-button">
+                        <b>Search</b>
+                        <i className="fa fa-search" />
+                    </button>
+                </div>
             </div>
-        </>
+        </div>
     );
 };
-
-export default Generic;
