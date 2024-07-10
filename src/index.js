6eb3b39f6d6612fceff578e2251f3099349fb6aa
JainDiya <diyaj9612@gmail.com>
2024-07-10 20:25:53 +0530

    adding

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import Signup from './Components/Signup';
+import Login from './Components/Login';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Signup/>
+    <Login/>
   </React.StrictMode>
 );
 
