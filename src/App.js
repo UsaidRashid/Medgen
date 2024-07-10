55fbfae83ac3aa4604c8c83c51377081bb13a79f
AnubhavO9 <anubhavsengar007@gmail.com>
2024-07-10 21:14:55 +0530

    adding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 import StoreRegistrationForm from './Components/StoreRegistrationForm';
-
+import RequestForm from './Components/RequestForm';
 function App() {
   return (
     <div className="App">
-     <StoreRegistrationForm />
+     <RequestForm />
     </div>
   );
 }
