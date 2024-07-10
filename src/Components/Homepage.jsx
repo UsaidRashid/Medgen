1b5084f7a58f2c54408aaa7d894e7055135df21d
UsaidRashid <rashidusaid2@gmail.com>
2024-07-10 18:27:13 +0530

    Pushing

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -6,9 +6,7 @@ import medilo from "../images/medilo.png";
 export default function Homepage(){
     return(
         <div>
-            
               <img className="w-100 h-100" style={{borderRadius:'0'}} src={background2} alt="home"/>
-
             <div className="d-flex flex-row justify-content-between bg-dark">
                 <div className="p-2 mx-5">
                 <img className="store" src={map} alt=""/>
@@ -18,11 +16,7 @@ export default function Homepage(){
                 <img className="med" src={medilo} alt=""/>
                 <h6 className="text-white">Medilo</h6>
                 </div>
-                
-               
             </div>
-               
         </div>           
-            
     );
 }
