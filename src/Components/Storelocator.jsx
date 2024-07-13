17a7741de9cd85c421b551d2288d317e5776ad60
ritik457 <ritikbansal457@gmail.com>
2024-07-13 21:03:14 +0530

    adding

--- src/Components/Storelocator.jsx
+++ src/Components/Storelocator.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../CSS/Storelocator.css';
+//import '../CSS/Storelocator.css';//
 
 function Storelocator() {
   const [stores, setStores] = useState([
@@ -39,22 +39,30 @@ function Storelocator() {
   ]);
 
   return (
-    <div className="Store">
-      <h1>Store Locator</h1>
-      <div className="store-list mt-4"> 
-      <div class="md-form mt-2">
-  <i class="material-icons mdc-button__icon"></i>
+
+    <div className="Store text-dark  " style={{backgroundColor:"#e6ffff"}}>
+      <h1 className="text-center fw-bold">Store Locator</h1>
+      <div class="md-form mt-2 w-50  shadow-lg" style={{marginLeft:"380px"}}>
+  
   <input class="form-control" id='larger' type="text" placeholder="&nbsp;&nbsp;&nbsp;Search" aria-label="Search"/>
 </div>
+      <div className=" "> 
+      
         <ul>
           {stores.map(store => (
-            <li key={store.id}>
-              <h3>{store.name}</h3>
-              <p><strong>Store ID:</strong> {store.id}</p>
-              <p><strong>Address:</strong> {store.address}</p>
-              <p><strong>Latitude:</strong> {store.latitude}</p>
-              <p><strong>Longitude:</strong> {store.longitude}</p>
-              <p><strong>Pincode:</strong> {store.pincode}</p>
+            <li className="my-3 hover fs-3 p-2 w-100 h-50  d-flex justify-content-center " key={store.id}>
+              <div class="card d-flex shadow-lg border-2 border-black bg-white" style={{width:"35rem"}}>
+  
+   <div class="card-body d-flex flex-column"style={{backgroundColor:"white"}}>
+    <h2 class="card-title text-center fw-bold font-monospace ">{store.name}</h2>
+    <p class="card-text"><strong>Store ID:</strong> {store.id}</p>
+    <p class="card-text"><strong>Address:</strong> {store.address}</p>
+    <p class="card-text"><strong>Latitude:</strong> {store.latitude}</p>
+    <p class="card-text"><strong>Longitude:</strong> {store.longitude}</p>
+    <p class="card-text"><strong>Pincode:</strong> {store.pincode}</p>
+  </div>
+</div>
+            
             </li>
           ))}
         </ul>
