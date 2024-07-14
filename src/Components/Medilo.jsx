89326f1c27fe4d3d2ad12f118f00f8723ef58f1c
Saksham Agarwal <sakshamagarwal0507@gmail.com>
2024-07-14 18:51:47 +0530

    Update both Compare and Search Pages

--- src/Components/Medilo.jsx
+++ src/Components/Medilo.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import background from '../Images/medilo-bg.png';
 import "../CSS/Medilo.css";
+import { useNavigate } from 'react-router-dom';
 
 const myStyle = {
   color: "Black",
@@ -23,6 +24,20 @@ const myStyle3 = {
 
 
 export default function Medilo() {
+  const navigate = useNavigate();
+
+    const openBSearch = (e) =>{
+        e.preventDefault();
+        navigate('/branded-search');
+    }
+
+  const navigate1 = useNavigate();
+
+    const openGSearch = (e) =>{
+        e.preventDefault();
+        navigate1('/generic-search');
+    }
+
   return (
     <>
       <div className=' d-flex align-items-center p-5 justify-content-left' style={{ backgroundImage: `url(${background})`, backgroundRepeat: "no-repeat", backgroundSize: "contain", backgroundPosition: "right", height: '100vh', backgroundColor: "white" }}>
@@ -30,8 +45,8 @@ export default function Medilo() {
           <h1 className='text-center' style={myStyle2}><b>MEDILO</b></h1>
           <h1 className='text-center' style={{ color: "black" }}>Here you can</h1>
           <h1 className='text-center' style={myStyle3}>choose an option for search...</h1>
-          <button style={myStyle} className='btn mx-5 fs-4 b123'>Generic Search</button>
-          <button style={myStyle} className='btn mx-5 fs-4 b123'>Brand Search</button>
+          <button style={myStyle} className='btn mx-5 fs-4 b123' onClick={openGSearch}>Generic Search</button>
+          <button style={myStyle} className='btn mx-5 fs-4 b123' onClick={openBSearch}>Brand Search</button>
         </form>
       </div>
     </>
