753f3c8bb95bdcd03357b06016a240c695a3de50
Saksham Agarwal <sakshamagarwal0507@gmail.com>
2024-07-15 06:52:50 +0530

    merging from saksham

--- src/Components/Medilo.jsx
+++ src/Components/Medilo.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import background from '../Images/medilo-bg.png';
 import "../CSS/Medilo.css";
 import { useNavigate } from 'react-router-dom';
+import { useTypewriter } from 'react-simple-typewriter';
 
 const myStyle = {
   color: "Black",
@@ -17,8 +18,8 @@ const myStyle2 = {
 };
 
 const myStyle3 = {
-  color: "black",
-  marginBottom: "150px",
+  color: "#2380ea",
+  marginBottom: "110px",
   fontWeight: "500",
 };
 
@@ -26,25 +27,31 @@ const myStyle3 = {
 export default function Medilo() {
   const navigate = useNavigate();
 
-    const openBSearch = (e) =>{
-        e.preventDefault();
-        navigate('/branded-search');
-    }
+  const openBSearch = (e) => {
+    e.preventDefault();
+    navigate('/branded-search');
+  }
 
   const navigate1 = useNavigate();
 
-    const openGSearch = (e) =>{
-        e.preventDefault();
-        navigate1('/generic-search');
-    }
+  const openGSearch = (e) => {
+    e.preventDefault();
+    navigate1('/generic-search');
+  }
+
+  const [ text ] = useTypewriter({
+    words: ['earch for Generic Medicines', 'earch for Branded Medicines'],
+    loop: {},
+    delaySpeed: 100,
+  });
 
   return (
     <>
       <div className=' d-flex align-items-center p-5 justify-content-left' style={{ backgroundImage: `url(${background})`, backgroundRepeat: "no-repeat", backgroundSize: "contain", backgroundPosition: "right", height: '100vh', backgroundColor: "white" }}>
         <form>
           <h1 className='text-center' style={myStyle2}><b>MEDILO</b></h1>
-          <h1 className='text-center' style={{ color: "black" }}>Here you can</h1>
-          <h1 className='text-center' style={myStyle3}>choose an option for search...</h1>
+          <h1 className='text-center' style={{ color: "black" }}>Here you can...</h1>
+          <h1 className='text-center' style={myStyle3}>S{text}</h1>
           <button style={myStyle} className='btn mx-5 fs-4 b123' onClick={openGSearch}>Generic Search</button>
           <button style={myStyle} className='btn mx-5 fs-4 b123' onClick={openBSearch}>Brand Search</button>
         </form>
