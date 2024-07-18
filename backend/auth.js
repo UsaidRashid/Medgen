3deacaad029a07b09ad7c8723203cb33fe418f1b
UsaidRashid <rashidusaid2@gmail.com>
2024-07-18 18:27:50 +0530

    changes

--- backend/auth.js
+++ backend/auth.js
@@ -3,8 +3,6 @@ const passport = require('passport');
 var GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 
 passport.use(new GoogleStrategy({
-    // clientID,
-    // clientSecret,
     callbackURL: "http://localhost:3000/auth/google/callback",
     passReqToCallback   : true
   },
