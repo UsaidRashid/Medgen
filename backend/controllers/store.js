38519e91375e842e47da2184add03262e79f6648
Saksham Agarwal <sakshamagarwal0507@gmail.com>
2024-07-18 08:21:26 +0530

    backend registration form & store locator

--- backend/controllers/store.js
+++ backend/controllers/store.js
@@ -7,24 +7,22 @@ module.exports.storelocator = async (req, res) => {
 }
 
 module.exports.getAllStores = async (req, res) => {
-    const stores = await Store.find({});
-    res.json(stores);
+        const stores = await Store.find({});
+        res.json(stores);
 };
 
 module.exports.registerStore = async (req, res) => {
     const store = new Store({
-        storeId: req.body.storeId,
-        storeName: req.body.storeName,
-        latitude: req.body.latitude,
-        longitude: req.body.longitude,
-        pincode: req.body.pincode,
-        address: req.body.address,
-        contact: req.body.contact,
-        ownerName: req.body.ownerName,
-        residentialAddress: req.body.residentialAddress
-    });
-
-
-    const newStore = await store.save();
-    res.json(newStore);
+    storeId: req.body.storeID,
+    storeName: req.body.storeName,
+    latitude: req.body.latitude,
+    longitude: req.body.longitude,
+    pincode: req.body.pincode,
+    address: req.body.address,
+    contactNo: req.body.contactNo,
+    ownerName: req.body.ownerName,
+    residentialAddress: req.body.residentialAddress
+});
+const newStore = await store.save();
+res.json(newStore);
 };
