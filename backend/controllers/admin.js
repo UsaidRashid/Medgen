46d90ce82c4fdf1bc1743d03951ca05929115a83
ritik457 <ritikbansal457@gmail.com>
2024-07-18 17:42:58 +0530

    adding

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -1,28 +1,106 @@
-const brand=require('../models/brand');
+const brand=require('../models/brands');
+const store = require('../models/stores');
+const request = require('../models/requests');
+const generic = require('../models/generics');
 
 
 
-
-module.exports.addMedicine=async(req,res)=>{
+  module.exports.addBrand=async(req,res)=>{
+ try {
     const medicine={
-        medicine_name,
-        drug_code,
+        name,
+        code,
         salt,
-        batch_no,
-        mrp
+        batch,
+        price
     
     }=req.body;
-    const newMedicine=new brand(medicine);
-    await newMedicine.save();
+    const newBrand=new brand(medicine);
+    await newBrand.save();
     res.send(req.body);
-
-
 }
 
+catch (error) {
+    console.error(error);
+    return res.status(500).json({message:'Internal Server Error',error});
+}}
 
-module.exports.fetchMedicine = async (req,res)=>{
-   const response = await brand.find({});
+
+
+  module.exports.addGeneric=async(req,res)=>{
+ try{
+    const medicine={
+        name,
+        code,
+        salt,
+        batch,
+        price
+    
+    }=req.body;
+    const newGeneric=new generic(medicine);
+    await newGeneric.save();
+    res.send(req.body);
+ }catch (error) {
+    console.error(error);
+    return res.status(500).json({message:'Internal Server Error',error});
+ }}
+
+
+
+  module.exports.fetchRequest = async (req,res)=>{
+ try{
+    const response = await request.find({}); 
+    console.log(response);
+    res.send(response);
+ }catch (error) {
+    console.error(error);
+    return res.status(500).json({message:'Internal Server Error',error});
+ }}
+
+
+
+  module.exports.fetchStore=async(req,res)=>{
+ try{
+    const response = await store.find({});
   
-   console.log(response);
-   res.send(response);
- }
+    console.log(response);
+    res.send(response);
+ }catch (error) {
+    console.error(error);
+    return res.status(500).json({message:'Internal Server Error',error});
+ }}
+
+
+
+  module.exports.fetchDashboard=async(req,res)=>{
+ try{
+    const A = await generic.find({});
+    console.log(A);
+  
+    const B = await brand.find({});
+    console.log(B);
+
+    const C = await request.find({});
+    console.log(C);
+
+    const D = await store.find({});
+    console.log(B);
+ 
+    res.send({a:A.length,b:B.length,c:C.length,d:D.length});
+
+  }catch (error) {
+    console.error(error);
+    return res.status(500).json({message:'Internal Server Error',error});
+ }}
+
+
+
+  module.exports.deleteStore=async(req,res)=>{
+ try{
+    const result = await store.deleteOne({ _id:req.body._id});
+    console.log(result);
+    return res.status(200).json({message:'Successfully Deleted Store'});
+ }catch (error) {
+    console.error(error);
+    return res.status(500).json({message:'Internal Server Error',error});
+ }}
---
+++ backend/routes/admin.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const adminController = require('../controllers/admin');
+
+router
+    .route('/add-brand')
+        .post(adminController.addBrand);
+
+router
+    .route('/add-generic')
+        .post(adminController.addGeneric);
+
+
+router
+    .route('/fetch-requests')
+        .post(adminController.fetchRequest);
+
+router
+    .route('/fetch-stores')
+        .post(adminController.fetchStore);
+    
+router
+    .route('/fetch-dashboard')
+        .post(adminController.fetchDashboard);
+
+router
+    .route('/delete-store')
+            .post(adminController.deleteStore);        
+        
+
+       module.exports=router;
+
+       
