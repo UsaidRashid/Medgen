a966aff038ffa24515a5e7f6d926e60580257888
ritik457 <ritikbansal457@gmail.com>
2024-07-18 18:06:35 +0530

    adding

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -4,103 +4,99 @@ const request = require('../models/requests');
 const generic = require('../models/generics');
 
 
+module.exports.addBrand=async(req,res)=>{
+    try {
+        const medicine={
+            name,
+            code,
+            salt,
+            batch,
+            price
+            }=req.body;
+        const newBrand=new brand(medicine);
+        await newBrand.save();
+        return res.status(200).json({message:'Brand Medicine Added Successfully',newBrand});
 
-  module.exports.addBrand=async(req,res)=>{
- try {
-    const medicine={
-        name,
-        code,
-        salt,
-        batch,
-        price
+    } catch (error) {
+    console.error(error);
+    return res.status(500).json({message:'Internal Server Error',error});
+    }}
+
+
+
+module.exports.addGeneric=async(req,res)=>{
+    try{
+        const medicine={
+            name,
+            code,
+            salt,
+            batch,
+            price
     
-    }=req.body;
-    const newBrand=new brand(medicine);
-    await newBrand.save();
-    res.send(req.body);
-}
-
-catch (error) {
-    console.error(error);
-    return res.status(500).json({message:'Internal Server Error',error});
-}}
+        }=req.body;
+        const newGeneric=new generic(medicine);
+        await newGeneric.save();
+        return res.status(200).json({message:'Generic Medicine Added Successfully',newGeneric});
+    }catch (error) {
+        console.error(error);
+        return res.status(500).json({message:'Internal Server Error',error});
+    }}
 
 
 
-  module.exports.addGeneric=async(req,res)=>{
- try{
-    const medicine={
-        name,
-        code,
-        salt,
-        batch,
-        price
-    
-    }=req.body;
-    const newGeneric=new generic(medicine);
-    await newGeneric.save();
-    res.send(req.body);
- }catch (error) {
-    console.error(error);
-    return res.status(500).json({message:'Internal Server Error',error});
- }}
+module.exports.fetchRequest = async (req,res)=>{
+    try{
+        const response = await request.find({}); 
+        console.log(response);
+        return res.status(200).json({message:'Request Find Successfully',response});
+    }catch (error) {
+        console.error(error);
+        return res.status(500).json({message:'Internal Server Error',error});
+    }}
 
 
 
-  module.exports.fetchRequest = async (req,res)=>{
- try{
-    const response = await request.find({}); 
-    console.log(response);
-    res.send(response);
- }catch (error) {
-    console.error(error);
-    return res.status(500).json({message:'Internal Server Error',error});
- }}
+module.exports.fetchStore=async(req,res)=>{
+    try{
+        const response = await store.find({});
+        console.log(response);
+        return res.status(200).json({message:'Store Find Successfully',response});
+    }catch (error) {
+        console.error(error);
+        return res.status(500).json({message:'Internal Server Error',error});
+    }}
 
 
 
-  module.exports.fetchStore=async(req,res)=>{
- try{
-    const response = await store.find({});
+module.exports.fetchDashboard=async(req,res)=>{
+    try{
+        const A = await generic.find({});
+        console.log(A);
   
-    console.log(response);
-    res.send(response);
- }catch (error) {
-    console.error(error);
-    return res.status(500).json({message:'Internal Server Error',error});
- }}
+        const B = await brand.find({});
+        console.log(B);
 
-
-
-  module.exports.fetchDashboard=async(req,res)=>{
- try{
-    const A = await generic.find({});
-    console.log(A);
-  
-    const B = await brand.find({});
-    console.log(B);
-
-    const C = await request.find({});
-    console.log(C);
-
-    const D = await store.find({});
-    console.log(B);
+        const C = await request.find({});
+        console.log(C);
+   
+        const D = await store.find({});
+        console.log(B);
  
-    res.send({a:A.length,b:B.length,c:C.length,d:D.length});
+        return res.status(200).json({message:'Dashboard Found Successfully', a:A.length,b:B.length,c:C.length,d:D.length});
 
-  }catch (error) {
-    console.error(error);
-    return res.status(500).json({message:'Internal Server Error',error});
- }}
+    }catch (error) {
+        console.error(error);
+        return res.status(500).json({message:'Internal Server Error',error});
+    }}
 
 
 
-  module.exports.deleteStore=async(req,res)=>{
- try{
-    const result = await store.deleteOne({ _id:req.body._id});
-    console.log(result);
-    return res.status(200).json({message:'Successfully Deleted Store'});
- }catch (error) {
-    console.error(error);
-    return res.status(500).json({message:'Internal Server Error',error});
- }}
+module.exports.deleteStore=async(req,res)=>{
+    try{
+        const result = await store.deleteOne({ _id:req.body._id});
+        console.log(result);
+        return res.status(200).json({message:'Successfully Deleted Store'});
+    }catch (error) {
+        console.error(error);
+        return res.status(500).json({message:'Internal Server Error',error});
+    }}
