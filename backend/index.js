cf3d4e4c77d287095f0a85a518ce288580567854
AnubhavO9 <anubhavsengar007@gmail.com>
2024-07-19 20:57:21 +0530

    request form

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 6969;
+const cors = require('cors');
 
 const mongoose = require('mongoose');
 
@@ -22,6 +23,7 @@ const User = require('./models/users');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cors());
 
 
 const passport = require('passport');
