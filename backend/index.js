2e87542e365407503e207627dd43300535ac252c
vanshika299 <vg472352@gmail.com>
2024-07-19 10:03:32 -0700

    push

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 6969;
-
+const cors= require('cors');
 const mongoose = require('mongoose');
 
 const mongoUrl="mongodb://127.0.0.1:27017/Medgen";
@@ -22,6 +22,7 @@ const User = require('./models/users');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cors());
 
 
 const passport = require('passport');
