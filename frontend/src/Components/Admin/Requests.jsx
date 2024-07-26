8059741fa28fe1de8c87f6346d2e45b0d3706f35
UsaidRashid <rashidusaid2@gmail.com>
2024-07-26 12:35:59 +0530

    request by deeksha

--- frontend/src/Components/Admin/Requests.jsx
+++ frontend/src/Components/Admin/Requests.jsx
@@ -87,40 +87,10 @@ const Requests = () => {
 
   return (
 
-    <div>
-       
-        <div className="d-flex">
-            
-            <div>
-                <Sidebar/>
-                </div>
-                <div>
-            <h1 className="fs-2 mx-4">Medicine Request List</h1>
-            <table className="table table-striped table-hover  " style={{ width: '85vw', border: '0.2px' }}>
-                <thead>
-                    <tr>
-                        <th scope="col">Medicine Name</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Message</th>
-                    </tr>
-                </thead>
-                <tbody className="table-group-divider">
-                    {requests.map((request, index) => (
-                        <tr key={index}>
-                            <td >{request.medName}</td>
-                            <td>{request.name}</td>
-                            <td>{request.email}</td>
-                            <td>{request.message}</td>
-                            <td><button onClick={handleClick} className="text-center" type="button" style={{ height: "30px", width: "90px", backgroundColor: "#007bff", color: "#fff", border: "none", borderRadius: ".8rem", cursor: "pointer", boxShadow: "3px 3px 5px rgba(0, 0, 0, .31)", fontSize: "15px" }} >
-                                Response
-                            </button></td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-            
-          </div>
+      <React.Fragment>
+      <div className='d-flex flex-row bg-white'>
+        <div>
+          <Sidebar />
         </div>
         <div style={{ width: '85vw' }}>
           <DataTable
@@ -139,6 +109,7 @@ const Requests = () => {
           
         </div>
       </div>
+    </React.Fragment>
   );
 };
 
