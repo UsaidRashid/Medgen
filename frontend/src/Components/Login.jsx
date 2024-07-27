6dcb6527681c26b731c23b64725ed43ce126bc17
JainDiya <diyaj9612@gmail.com>
2024-07-27 20:03:06 +0530

    changes

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,143 +1,249 @@
-import React, { useState } from "react";
-import "../CSS/Login.css";
-import  {Link, useNavigate} from 'react-router-dom';
-import axios from 'axios';
+// import React, { useState } from "react";
+// import "../CSS/Login.css";
+// import  {Link, useNavigate} from 'react-router-dom';
+// import axios from 'axios';
 
-export default function Login() {
-    const navigate = useNavigate();
+// export default function Login() {
+//     const navigate = useNavigate();
 
-    const [formData , setFormData ] = useState({
-      username : '',
-      password : '',
-    });
+//     const [formData , setFormData ] = useState({
+//       username : '',
+//       password : '',
+//     });
     
-    const handleChange = (e) => {
-        setFormData({
-          ...formData,
-          [e.target.name]:e.target.value,
-        });
-    };
+//     const handleChange = (e) => {
+//         setFormData({
+//           ...formData,
+//           [e.target.name]:e.target.value,
+//         });
+//     };
 
-    const handleSubmit = async (e) =>{
-        e.preventDefault();
-        try{
-            const response = await axios.post('http://localhost:6969/users/login',formData);
+//     const handleSubmit = async (e) =>{
+//         e.preventDefault();
+//         try{
+//             const response = await axios.post('http://localhost:6969/users/login',formData);
 
-            if(response.status===200){
-                alert(response.data.message);
-                const token = response.data.token;          
-                localStorage.setItem('token',token);
-                navigate('/');
-            }else{
-                alert('Some Error occured :', response.data.message);
-            }
-        } catch (error) {
-            console.error("Error in Logging in:", error);
-            alert( `${error.name} -> ${error.message}`);
-            if (error.response) {
-              alert("Error from server: " + error.response.data.message);
-            } else if (error.request) {
-              alert("No response from the server");
-            } else {
-              alert("Error setting up the request: " + error.message);
-            }
-        }
-    }
+//             if(response.status===200){
+//                 alert(response.data.message);
+//                 const token = response.data.token;          
+//                 localStorage.setItem('token',token);
+//                 navigate('/');
+//             }else{
+//                 alert('Some Error occured :', response.data.message);
+//             }
+//         } catch (error) {
+//             console.error("Error in Logging in:", error);
+//             alert( `${error.name} -> ${error.message}`);
+//             if (error.response) {
+//               alert("Error from server: " + error.response.data.message);
+//             } else if (error.request) {
+//               alert("No response from the server");
+//             } else {
+//               alert("Error setting up the request: " + error.message);
+//             }
+//         }
+//     }
 
 
 
-  return (
-    <div className="container-fluid bg-white">
-      <div className="row">
-        <div className="col-md-6 text-center">
-          <h1 className="text-center mt-3 text-dark">
-            <b>Welcome</b>
-          </h1>
-          <h5 className="text-center mt-3 text-dark">
-            Login into your account
-          </h5>
-          <button
-            className="mt-5 bg-light text-dark p-3 border rounded"
-            type="button"
-            id="text1"
-          >
-            <b>
-              <a href="https://www.google.com/">Login with Google
-              <img src="image/removebg.png" className="img2" /></a>
-            </b>
-          </button>
+//   return (
+//     <div className="container-fluid bg-white">
+//       <div className="row">
+//         <div className="col-md-6 text-center">
+//           <h1 className="text-center mt-3 text-dark">
+//             <b>Welcome</b>
+//           </h1>
+//           <h5 className="text-center mt-3 text-dark">
+//             Login into your account
+//           </h5>
+//           <button
+//             className="mt-5 bg-light text-dark p-3 border rounded"
+//             type="button"
+//             id="text1"
+//           >
+//             <b>
+//               <a href="https://www.google.com/">Login with Google
+//               <img src="image/removebg.png" className="img2" /></a>
+//             </b>
+//           </button>
 
-          <div className="container">
-            <div className="row">
-              <div className="col-md-1"></div>
-              <div className="col-md-10">
-                <form onSubmit={handleSubmit}>
-                  <div class="form-group mt-4 mb-5 ">
-                    <input
-                      type="text"
-                      className="form-control border border-3 border-black p-4 border rounded-5 p-4 h3"
-                      placeholder="Username"
-                      name="username"
-                      value={formData.username}
-                      onChange={handleChange}
-                      required
-                    />
-                  </div>
-                  <div class="form-group mt-4 mb-3">
-                    <input
-                      type="password"
-                      name="password"
-                      className="form-control  border border-3 border-black border rounded-5 p-4 h3"
-                      placeholder="Password"
-                      value={formData.password}
-                      onChange={handleChange}
-                      required
-                    />
-                  </div>
-                  <div className="container">
-                    <div className="row">
-                      <div className="col-md-4">
-                        <label className="form-check-label text-dark h5">
+//           <div className="container">
+//             <div className="row">
+//               <div className="col-md-1"></div>
+//               <div className="col-md-10">
+//                 <form onSubmit={handleSubmit}>
+//                   <div class="form-group mt-4 mb-5 ">
+//                     <input
+//                       type="text"
+//                       className="form-control border border-3 border-black p-4 border rounded-5 p-4 h3"
+//                       placeholder="Username"
+//                       name="username"
+//                       value={formData.username}
+//                       onChange={handleChange}
+//                       required
+//                     />
+//                   </div>
+//                   <div class="form-group mt-4 mb-3">
+//                     <input
+//                       type="password"
+//                       name="password"
+//                       className="form-control  border border-3 border-black border rounded-5 p-4 h3"
+//                       placeholder="Password"
+//                       value={formData.password}
+//                       onChange={handleChange}
+//                       required
+//                     />
+//                   </div>
+//                   <div className="container">
+//                     <div className="row">
+//                       <div className="col-md-4">
+//                         <label className="form-check-label text-dark h5">
                           
-                          <input
-                            class="form-check-input"
-                            type="checkbox"
-                            id="flexSwitchCheckChecked"
-                            checked
-                          />
-                          <b>Remember me</b>
-                        </label>
-                      </div>
-                      <div className="col-md-4"></div>
-                      <div className="col-md-4">
-                        <label className="text-dark h5">
-                          <b>Recover Password</b>
-                        </label>
-                      </div>
-                    </div>
-                  </div>
+//                           <input
+//                             class="form-check-input"
+//                             type="checkbox"
+//                             id="flexSwitchCheckChecked"
+//                             checked
+//                           />
+//                           <b>Remember me</b>
+//                         </label>
+//                       </div>
+//                       <div className="col-md-4"></div>
+//                       <div className="col-md-4">
+//                         <label className="text-dark h5">
+//                           <b>Recover Password</b>
+//                         </label>
+//                       </div>
+//                     </div>
+//                   </div>
 
-                  <button
-                    type="Login"
-                    className="mt-2 bg-light p-3 border rounded-5 w-50 float-centre h4 shadow-lg shadow-white bg-dark text-white"
-                  >
-                    Login
-                  </button>
-                </form>
-              </div>
-              <div className="col-md-1"></div>
-            </div>
-            <h4>Already have an account ? <Link to="/Signup">Signup</Link></h4>
-          </div>
-        </div>
+//                   <button
+//                     type="Login"
+//                     className="mt-2 bg-light p-3 border rounded-5 w-50 float-centre h4 shadow-lg shadow-white bg-dark text-white"
+//                   >
+//                     Login
+//                   </button>
+//                 </form>
+//               </div>
+//               <div className="col-md-1"></div>
+//             </div>
+//             <h4>Already have an account ? <Link to="/Signup">Signup</Link></h4>
+//           </div>
+//         </div>
+//         <div className="col-md-6">
+//           <img
+//             src="image/ss.png"
+//             alt=""
+//             className="img-fluid w-100 h-600"
+//           ></img>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+
+import React, { useState } from 'react'
+import axios from 'axios';
+import { useNavigate,Link } from 'react-router-dom';
+
+
+function Login() {
+  const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+      
+      username: "",
+      password: ""
+    });
+  
+  
+    const handleChange = (e) => {
+      setFormData({
+        ...formData,
+        [e.target.name] : e.target.value
+      })
+    }
+  
+    const handleSubmit = async (e) => {
+      try {
+        e.preventDefault();
+        console.log(formData);
+  
+        const response = await axios.post('http://localhost:6969/users/signup',formData);
+        
+  
+        if( response.status===200){
+            alert(response.data.message);
+            const token = response.data.token;
+            localStorage.setItem('token',token);
+            navigate("/");
+        }else{
+            alert('There was a problem in signing up the user....',response.data.message);
+            navigate('/login')
+        }      
+  
+      } catch (error) {
+          console.error("Error in Registering:", error);
+          alert( `${error.name} -> ${error.message}`);
+          if (error.response) {
+            alert("Error from server: " + error.response.data.message);
+          } else if (error.request) {
+            alert("No response from the server");
+          } else {
+            alert("Error setting up the request: " + error.message);
+          }
+      }
+    };
+  
+  return (
+    <>
+    
+    <section>
+      <div className="container mt-3  mb-3">
+        <div className="row">
         <div className="col-md-6">
-          <img
-            src="image/ss.png"
-            alt=""
-            className="img-fluid w-100 h-600"
-          ></img>
+            <img src="image/login.png" className='w-100 h-100' alt="" />
+          </div>
+          <div className="col-md-6 shadow-lg mb-3">
+            <h1 className='text-center mt-3 '><b>Welcome</b></h1>
+            <h5 className='text-center mt-3 '><b>Login into your account</b></h5>
+            <form className="mx-5 mb-5 mt-5 " onSubmit={handleSubmit}>
+  
+  <div className="form-group mt-3">
+    <label >Username</label>
+    <input type="name" className="form-control mb-1"  name='username' value={formData.username} onChange={handleChange}/>
+  </div>
+
+  <div className="form-group">
+    <label>Password</label>
+    <input type="password" className="form-control mb-1"   name='password'value={formData.password} onChange={handleChange} />
+  </div>
+
+  
+  <button
+                type="submit"
+                className="mt-2 bg-light p-3 border rounded-3  float-centre  shadow-lg shadow-white bg-dark text-white"
+              >
+                <b>Login</b>
+              </button>
+              <h4 className="text-center mt-3 text-black">
+                 Don't have an account?
+                 <b>
+                   <Link to="/signup">Signup</Link>
+                 </b>
+               </h4>
+  
+</form>
+          </div>
+          
         </div>
+          
+          
       </div>
-    </div>
-  );
+    </section>
+    </>
+  )
 }
+
+export default Login;
