b741ab23ecca10e1cf07aaab2d593ca6ffaf2673
UsaidRashid <rashidusaid2@gmail.com>
2024-08-01 13:58:16 +0530

    changes

--- backend/configs/multerConfig.js
+++ backend/configs/multerConfig.js
@@ -1,32 +1,30 @@
-const multer = require('multer');
+const multer = require("multer");
 // const path = require('path');
 // const fs = require('fs');
-const cloudinary = require('cloudinary').v2;
-const {CloudinaryStorage} = require("multer-storage-cloudinary");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 cloudinary.config({
-  cloud_name: 'dnys9p4ar',
-  api_key: '168144747537394',
-  api_secret: '4FcS_4EsC7Ban9cOgxcT-31SKYQ'
+  cloud_name: "dnys9p4ar",
+  api_key: "168144747537394",
+  api_secret: "4FcS_4EsC7Ban9cOgxcT-31SKYQ",
 });
-
 
 // const uploadDir = path.join(__dirname, 'uploads');
 
 // if (!fs.existsSync(uploadDir)) {
-//   fs.mkdirSync(uploadDir); 
+//   fs.mkdirSync(uploadDir);
 // }
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
-  params:{
+  params: {
     folder: "Medgen",
-    allowedFormats: ["png","jpg","jpeg"],
-},
-  // folder: 'Medgen',
+    allowedFormats: ["png", "jpg", "jpeg"],
+  },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
-  }
+  },
 });
 
 const upload = multer({ storage });
